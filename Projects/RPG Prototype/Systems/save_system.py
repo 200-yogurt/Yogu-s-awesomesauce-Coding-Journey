ab3cb921d288bf72e8e3d6d7a62ac481f5ec3340
the forgotten yogurt <65128443+200-yogurt@users.noreply.github.com>
2026-06-13 16:45:02 -0500

    Save system overhaul.

--- Projects/RPG Prototype/Systems/save_system.py
+++ Projects/RPG Prototype/Systems/save_system.py
@@ -2,14 +2,39 @@ import os
 from config import *
 from Systems.utils import narrate, intro_cutscene
 
+def save_player_data():
+    
+    with open(saveFilePath, "w") as file:
+        file.write("- - PLAYER DATA - -")
+        
+        file.write("\n\nPlayer Stats :\n")
+        for key, value in playerData["stats"].items():
+            file.write(f"   {key} :    {value}\n")
+
+        file.write("\n\nPlayer Inventory :\n")
+        for item in playerData["inventory"]:
+                file.write(f"   {item}\n")
+
 def load_save_data():
+
     if os.path.exists(saveFilePath):
+
         with open(saveFilePath, "r") as file:
+
             for line in file:
-                key, value = line.replace(" ", "").strip().split(":")
-                if key in ("HEALTH", "GOLD"):
-                    value = int(value)
-                playerData[key] = value
+
+                if line == "Player Stats :":
+                    print("Player data stats found")
+                    key, value = line.replace(" ", "").strip().split(":")
+                    if key in ("HEALTH", "GOLD"):
+                        value = int(value)
+                    playerData["stats"][key] = value
+                    
+                if line == "Player Inventory :":
+                    print("Player data inventory found")
+                    value = line.strip()
+                    playerData["inventory"] = value
+            
         print("\nSuccesfully Found & Loaded Save File")
         narrate("Loading. . .", speed["slow"])
 
@@ -18,5 +43,4 @@ def load_save_data():
         intro_cutscene("new")
 
         with open (saveFilePath, "x") as file:
-            for key, value in playerData.items():
-                file.write(f"{key} :    {value}\n")
+            save_player_data()
